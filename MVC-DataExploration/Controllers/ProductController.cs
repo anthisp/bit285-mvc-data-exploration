34966c95e670ebd48975437a8c28f91cdeb333bd
Logik <Logik@DESKTOP-17O0KAC>
2017-01-24 14:39:34 -0800

    Complete

---
+++ MVC-DataExploration/Controllers/ProductController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC_DataExploration.Models;
+
+namespace MVC_DataExploration.Controllers
+{
+    public class ProductController : Controller
+    {
+        private MyDBContext db = new MyDBContext();
+ 
+        // GET: Product
+        public ActionResult Index()
+        {
+            
+            return View("Index",db.Products);
+        }
+    }
+}
