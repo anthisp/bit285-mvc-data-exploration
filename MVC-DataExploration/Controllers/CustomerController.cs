a5d1b55322a1f87f6d1fe08f304be405b11a2b52
Logik <Logik@DESKTOP-17O0KAC>
2017-01-24 12:38:18 -0800

    l

--- MVC-DataExploration/Controllers/CustomerController.cs
+++ MVC-DataExploration/Controllers/CustomerController.cs
@@ -16,5 +16,26 @@ namespace MVC_DataExploration.Controllers
         {
             return View(db.Customers);
         }
+        
+        //GET Create  - Sends view with boxes  to input
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View(); //Show the form
+        }
+
+        //POST Create - Work with the model to update database 
+        [HttpPost]
+        public ActionResult Create(Customer myCustomer)
+        {
+            //Add new customer to set of Customeers
+            db.Customers.Add(myCustomer);
+            //Updates the database
+            db.SaveChanges();
+            return View("Index", db.Customers); // Show updated Index Page
+        }
+
+
+
     }
 }
